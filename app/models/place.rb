05d781b1c66ef9763d69e43e7f637a95e1f9eee2
Nate <primesitemedia@gmail.com>
2014-10-28 16:29:37 +0800

    remove scaffold.css, add 4 Places to db, add Geocoding the latitude/longitude, setup /map url

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,12 @@
 class Place < ActiveRecord::Base
+
+  # Geocoding the latitude/longitude
+
+  # When we create the new venue, we don't know the latitude and longitude. Thus, we want to provide the address,
+  # and use some 3rd party service to convert it to lat/lng
+
+  reverse_geocoded_by :lat, :lng
+  after_validation :reverse_geocode  # auto-fetch address
+  geocoded_by :address, :latitude  => :lat, :longitude => :lng   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
 end
